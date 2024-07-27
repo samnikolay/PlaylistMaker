f43b5a2e104750e7c2019b306612e1e343f0c847
samnikolay <sam.nikolay@gmail.com>
2024-07-27 15:13:11 +0300

    First commit PLM

--- app/src/main/java/com/samnic/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/samnic/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,26 @@
 package com.samnic.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_settings : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Получаем id кнопки из ресурсов
+        val backButton = findViewById<ImageView>(R.id.arrow_back)
+
+        backButton.setOnClickListener {
+            val mainsIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainsIntent)
         }
     }
 }
