269ff0f217e010c3cab5d85d2a38dd638f4deae4
samnikolay <sam.nikolay@gmail.com>
2024-07-28 12:26:37 +0300

    First commit PLM

--- app/src/main/java/com/samnic/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/samnic/playlistmaker/SearchActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
 
--- app/src/main/java/com/samnic/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/samnic/playlistmaker/SettingsActivity.kt
@@ -12,7 +12,6 @@ import androidx.core.view.WindowInsetsCompat
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
         // Получаем id кнопки из ресурсов
