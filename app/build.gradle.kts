431729229213a42db89ce54cb042097faab21b9b
samnikolay <sam.nikolay@gmail.com>
2024-08-08 16:59:42 +0300

    Followed some of the reviewer's recommendations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.samnic.playlistmaker"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
